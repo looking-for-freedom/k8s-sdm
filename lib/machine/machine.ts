3449353d6ea695a01468ad14f6174979d109e08e
David Dooling <david@atomist.com>
2018-12-07 14:12:55 -0600

    Support building itself
    It would be nice if k8-sdm and its progeny could build and redeploy
    itself.  This commit adds build and Docker build support for Node.js
    projects.
    
    Add a generator command using this project as a seed.

--- lib/machine/machine.ts
+++ lib/machine/machine.ts
@@ -15,26 +15,99 @@
  */
 
 import {
+    GitHubRepoRef,
+} from "@atomist/automation-client";
+import {
+    goals,
     SoftwareDeliveryMachine,
     SoftwareDeliveryMachineConfiguration,
+    whenPushSatisfies,
 } from "@atomist/sdm";
 import {
     createSoftwareDeliveryMachine,
+    isInLocalMode,
+    Version,
 } from "@atomist/sdm-core";
+import { Build } from "@atomist/sdm-pack-build";
+import {
+    DefaultDockerImageNameCreator,
+    DockerBuild,
+    DockerOptions,
+    HasDockerfile,
+} from "@atomist/sdm-pack-docker";
 import {
     kubernetesSupport,
 } from "@atomist/sdm-pack-k8";
+import {
+    IsNode,
+    nodeBuilder,
+    NodeModulesProjectListener,
+    NodeProjectCreationParametersDefinition,
+    NodeProjectVersioner,
+    UpdatePackageJsonIdentification,
+    UpdateReadmeTitle,
+} from "@atomist/sdm-pack-node";
 
 export function machine(
     configuration: SoftwareDeliveryMachineConfiguration,
 ): SoftwareDeliveryMachine {
 
     const sdm = createSoftwareDeliveryMachine({
-        name: "In-Cluster Kubernetes Software Delivery Machine",
+        name: "Kubernetes Software Delivery Machine",
         configuration,
     });
 
+    const version = new Version().with({
+        name: "npm-versioner",
+        versioner: NodeProjectVersioner,
+        pushTest: IsNode,
+    });
+    const build = new Build().with({
+        name: "npm-run-build",
+        builder: nodeBuilder("npm run compile", "npm test"),
+        pushTest: IsNode,
+    })
+        .withProjectListener(NodeModulesProjectListener);
+    const buildGoals = goals("build")
+        .plan(version)
+        .plan(build).after(version);
+
+    const dockerBuildOptions: DockerOptions = { push: false };
+    if (!isInLocalMode() && sdm.configuration.sdm && sdm.configuration.sdm.docker) {
+        const d = sdm.configuration.sdm.docker;
+        if (d.registry && d.user && d.password) {
+            dockerBuildOptions.push = true;
+            dockerBuildOptions.registry = d.registry;
+            dockerBuildOptions.user = d.user;
+            dockerBuildOptions.password = d.password;
+        }
+    }
+    const dockerBuild = new DockerBuild().with({
+        name: "npm-docker-build",
+        imageNameCreator: DefaultDockerImageNameCreator,
+        options: dockerBuildOptions,
+        pushTest: IsNode,
+    });
+    const dockerGoals = goals("docker")
+        .plan(dockerBuild).after(buildGoals);
+
+    sdm.withPushRules(
+        whenPushSatisfies(IsNode).setGoals(buildGoals),
+        whenPushSatisfies(HasDockerfile).setGoals(dockerGoals),
+    );
+
     sdm.addExtensionPacks(kubernetesSupport());
+
+    sdm.addGeneratorCommand({
+        name: "k8-sdm-generator",
+        startingPoint: new GitHubRepoRef("atomist", "k8-sdm"),
+        intent: "create k8-sdm",
+        parameters: NodeProjectCreationParametersDefinition,
+        transform: [
+            UpdatePackageJsonIdentification,
+            UpdateReadmeTitle,
+        ],
+    });
 
     return sdm;
 }
